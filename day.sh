131b692342b0822118a5ec7f0179269cb9ebfdb6
ympek <sz@ympek.net>
2022-12-06 20:43:13 +0100

    added launcher for the puzzle solutions, since they are being written in distinct languages

---
+++ day.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! $1 ] 
+then
+  echo "You need to specify day number to run, e.g. ./day.sh 1 will run the code from day 1 folder."
+  exit 1
+fi
+
+case $1 in
+  1)
+    node day1
+    ;;
+  2)
+    node day2
+    ;;
+  3)
+    pushd day3 > /dev/null
+    go run main.go
+    popd > /dev/null
+    ;;
+  4)
+    npx ts-node-esm day4/main.ts
+    ;;
+  *)
+    echo "Invalid or not yet finished day."
+    ;;
+esac
